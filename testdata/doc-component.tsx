795115ba0d8d4368b17a52286c8939cc2230b4b6
Takin Profit <techstar.dev@hotmail.com>
2026-02-08 23:32:46 -0500

    fix: make testdata doc-component.tsx a realistic TSX documentation component

--- testdata/doc-component.tsx
+++ testdata/doc-component.tsx
@@ -1,19 +1,22 @@
-<h1>Button Component</h1>
+import React from 'react';
 
-<p>A reusable button component.</p>
+export default function ButtonDocs() {
+  return (
+    <div className="docs">
+      <h1>Button Component</h1>
+      <p>A reusable button component for your application.</p>
 
-<h2>Props</h2>
+      <h2>Props</h2>
+      <ul>
+        <li><code>variant</code>: Button style variant</li>
+        <li><code>onClick</code>: Click handler function</li>
+      </ul>
 
-<ul>
-  <li><code>variant</code>: Button style</li>
-  <li><code>onClick</code>: Click handler</li>
-</ul>
-
-<h2>Example</h2>
-
-<Example>
-  <Button variant="primary" onClick={handleClick}>
-    Click me
-  </Button>
-</Example>
+      <h2>Examples</h2>
+      <pre>
+        {`<Button variant="primary">Click me</Button>`}
+      </pre>
+    </div>
+  );
+}
 
